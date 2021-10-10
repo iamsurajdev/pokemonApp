cf5440dec07e782f549336c3f2cd17b30b29d442
Suraj Biswas <surajbiswas367@gmail.com>
2021-10-10 17:34:51 +0530

    get the pokemon data and showing on the screen

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,41 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import SEO from "@components/SEO";
 import axios from "@services/axiosConfig";
 import { getAllPokemon } from "@services/api";
 
+type fetchedData = {
+  results: resultsType;
+  count: number;
+};
+
+type resultsType = [
+  {
+    name: string;
+    url: string;
+    data?: any;
+  }
+];
+
 export default function Home() {
+  const [allPokemon, setAllPokemon] = useState<resultsType[] | any>();
+
   const fetchPokemon = async () => {
-    const data = (await axios.get(getAllPokemon(20))).data;
-    console.log(data);
+    try {
+      const data: fetchedData = (await axios.get(getAllPokemon(10))).data;
+      const { results } = data;
+      const d: resultsType[] | any = await Promise.all(
+        results.map(async (i) => {
+          const res = await axios.get(i.url);
+          console.log(res);
+
+          return { ...i, data: res?.data };
+        })
+      );
+      console.log(d);
+      setAllPokemon(d);
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   useEffect(() => {
@@ -21,6 +50,18 @@ export default function Home() {
       />
       <main>
         <h1>Pokemon</h1>
+        <div>
+          {allPokemon &&
+            allPokemon?.map((pokemon: resultsType | any) => (
+              <div key={pokemon?.data?.id}>
+                <p>name : {pokemon.name}</p>
+                <img
+                  src={pokemon.data.sprites.other.dream_world.front_default}
+                  alt={pokemon.name}
+                />
+              </div>
+            ))}
+        </div>
       </main>
     </div>
   );
