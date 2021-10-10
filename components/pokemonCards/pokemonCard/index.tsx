c8bb955db5f56a77ce3512f5683e49568c9d2df7
Suraj Biswas <surajbiswas367@gmail.com>
2021-10-11 00:21:40 +0530

    fetching pokemon details for each pokemon

--- components/pokemonCards/pokemonCard/index.tsx
+++ components/pokemonCards/pokemonCard/index.tsx
@@ -1,9 +1,31 @@
-import { pokemonListSingleItemType } from "@lib/types/pokemon";
+import axios from "@lib/services/axiosConfig";
+import { pokemonDetails, pokemonListSingleItemType } from "@lib/types/pokemon";
+import { useEffect, useState } from "react";
 import styles from "./style.module.css";
 
 const PokemonCard: React.FC<{
   pokemon: pokemonListSingleItemType;
 }> = ({ pokemon }) => {
+  const [details, setDetails] = useState<pokemonDetails>();
+
+  const fetchPokemonDetails = async () => {
+    const response: pokemonDetails = (
+      await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`)
+    ).data;
+
+    const finalData: pokemonDetails = {
+      height: response.height,
+      weight: response.weight,
+      types: response.types,
+    };
+
+    setDetails(finalData);
+  };
+
+  useEffect(() => {
+    fetchPokemonDetails();
+  }, []);
+
   return (
     <div className={styles.cardContainer}>
       <p className="font-bold h-8 text-center capitalize">{pokemon.name}</p>
