cc6f2eab49c0baf091cf8eb2ed74644bbce9ba3f
Suraj Biswas <surajbiswas367@gmail.com>
2021-10-12 00:06:41 +0530

    minor fix

--- lib/services/api.ts
+++ lib/services/api.ts
@@ -2,5 +2,3 @@ export const getAllPokemon = (limit: number, offset: number) =>
   `pokemon?limit=${limit}&offset=${offset}`;
 export const pokemonImageURL = (id: string) =>
   `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${id}.svg`;
-
-export default { getAllPokemon };
