89aad6967a8571b5dea7dd377edc1a7ff9198446
Suraj Biswas <surajbiswas367@gmail.com>
2021-10-12 01:02:14 +0530

    modal completed with styling

--- components/pokemonCards/index.tsx
+++ components/pokemonCards/index.tsx
@@ -38,24 +38,46 @@ const PokemonCards: React.FC<{ pokemons: pokemonListSingleItemType[] }> = ({
           src={modalInfo?.imageUrl}
           alt={modalInfo?.name}
         />
-        <p>
-          <span className="font-bold">Name :</span> {modalInfo?.name}
-        </p>
-        <p>
-          <span className="font-bold">Type : </span> {modalInfo?.type} &nbsp;
-          {pokemonTypesCheck[modalInfo?.type]?.icon}
-        </p>
-        <p>
-          <span className="font-bold">Base experience :</span>
-          {modalInfo?.baseExperience}
-        </p>
-        <p>
-          <span className="font-bold">Height : </span> {modalInfo?.height}
-        </p>
-        <p>
-          <span className="font-bold">Weight : </span>
-          {modalInfo?.weight}
-        </p>
+        <div className="w-full flex justify-around items-center flex-wrap mt-7">
+          <p>
+            <span className="font-bold">Name :</span> {modalInfo?.name}
+          </p>
+          <p>
+            <span className="font-bold">Type : </span> {modalInfo?.type} &nbsp;
+            {pokemonTypesCheck[modalInfo?.type]?.icon}
+          </p>
+        </div>
+        <div className="w-full flex justify-around items-center flex-wrap mt-5">
+          <p>
+            <span className="font-bold">Base experience : </span>
+            {modalInfo?.baseExperience}
+          </p>
+          <p>
+            <span className="font-bold">Height : </span> {modalInfo?.height}
+          </p>
+          <p>
+            <span className="font-bold">Weight : </span>
+            {modalInfo?.weight}
+          </p>
+        </div>
+        <div
+          className="border-2 border-solid rounded-md mt-3"
+          style={{ borderColor: pokemonTypesCheck[modalInfo?.type]?.color }}
+        >
+          <p className="text-xl font-semibold ml-4">Top Moves</p>
+          <div className="w-full flex justify-around items-center flex-wrap mb-2">
+            {modalInfo?.topMoves.map((item) => (
+              <span
+                className="p-2 mt-2 capitalize rounded-md"
+                style={{
+                  backgroundColor: pokemonTypesCheck[modalInfo?.type]?.color,
+                }}
+              >
+                {item}
+              </span>
+            ))}
+          </div>
+        </div>
       </Modal>
     </>
   );
